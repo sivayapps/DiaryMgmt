4e6d14ebfb507e40d4c4210f8beb583ebbcb0524
shivamahi <shivakumar.alapaka@gmail.com>
2018-02-03 23:04:52 +0530

    bug fix on DairyOwnerProfileSetupActivity and domain class RateEntry Fields added and added and some miscellaneous changes like formating

--- app/src/main/java/in/boshanam/diarymgmt/domain/RateEntry.java
+++ app/src/main/java/in/boshanam/diarymgmt/domain/RateEntry.java
@@ -9,18 +9,48 @@ import android.widget.Spinner;
 
 public class RateEntry {
     private String milkType;
-    private String fat;
-    private String rate;
+    private float fat;
+    private float rate;
+    private Long date;
+    private String shift;
 
-
-    public RateEntry(String milkType, String fat,String rate) {
-        this.milkType=milkType;
-        this.fat=fat;
-        this.rate=rate;
-
+    public String getMilkType() {
+        return milkType;
     }
 
+    public void setMilkType(String milkType) {
+        this.milkType = milkType;
+    }
 
+    public float getFat() {
+        return fat;
+    }
 
+    public void setFat(float fat) {
+        this.fat = fat;
+    }
 
+    public float getRate() {
+        return rate;
+    }
+
+    public void setRate(float rate) {
+        this.rate = rate;
+    }
+
+    public Long getDate() {
+        return date;
+    }
+
+    public void setDate(Long date) {
+        this.date = date;
+    }
+
+    public String getShift() {
+        return shift;
+    }
+
+    public void setShift(String shift) {
+        this.shift = shift;
+    }
 }
