d050362c6ecbdc69c87fb8ca463e2b27ac793399
sivayapps <sivayapps>
2018-02-07 22:27:06 +0530

    rate domain model

--- app/src/main/java/in/boshanam/diarymgmt/domain/Rate.java
+++ app/src/main/java/in/boshanam/diarymgmt/domain/Rate.java
@@ -3,14 +3,43 @@ package in.boshanam.diarymgmt.domain;
 import android.widget.EditText;
 import android.widget.Spinner;
 
+import java.util.Date;
+
 /**
  * Created by user on 2/2/2018.
  */
 
 public class Rate {
+    private String id;
+    private String dairyId;
     private String milkType;
     private float fat;
     private float rate;
+    private Date effectiveDate;
+
+    public String getDairyId() {
+        return dairyId;
+    }
+
+    public void setDairyId(String dairyId) {
+        this.dairyId = dairyId;
+    }
+
+    public String getId() {
+        return id;
+    }
+
+    public void setId(String id) {
+        this.id = id;
+    }
+
+    public Date getEffectiveDate() {
+        return effectiveDate;
+    }
+
+    public void setEffectiveDate(Date effectiveDate) {
+        this.effectiveDate = effectiveDate;
+    }
 
     public String getMilkType() {
         return milkType;
