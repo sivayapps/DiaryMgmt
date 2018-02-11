3159da8351af54301796e7bb18bb1b7e56ac509a
shivamahi <shivakumar.alapaka@gmail.com>
2018-02-11 21:43:01 +0530

    FramerReportActivity Class created

---
+++ app/src/main/java/in/boshanam/diarymgmt/FarmerReportActivity.java
@@ -0,0 +1,14 @@
+package in.boshanam.diarymgmt;
+
+import android.os.Bundle;
+import android.app.Activity;
+
+public class FarmerReportActivity extends Activity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_farmer_reports);
+    }
+
+}
